64ffcfc32b5cf2deb21c6fbc72083a5bf8cd78ad
Alex Hughes <alex.hughes@interapthq.com>
2020-06-09 20:38:21 -0400

    award images

--- src/Router.js
+++ src/Router.js
@@ -7,6 +7,7 @@ import CustomerMain from './components/CustomerMain';
 import SignUp from './components/SignUp';
 import RiderStats from './components/riderStats';
 import ResetPassword from './components/ResetPassword';
+import i18n from "i18n-js";
 import { E_MAIN_TITLE, MAIN_TITLE, E_REGISTER, REGISTER, WORKING, JOB_LIST } from './LanguageFile';
 
 const RouterComponent = () => {
@@ -15,19 +16,19 @@ const { headerColor, headerText } = styles;
     <Router navigationBarStyle={ headerColor } titleStyle={{ color: '#fff'}} backTitle=" " navBarButtonColor='#fff' >
       <Scene key='root' hideNavBar>
         <Scene key='auth'>
-          <Scene key='login' component={LogonForm} title={MAIN_TITLE} titleStyle={ headerText } />
-          <Scene key='signUp' component={SignUp} title={REGISTER} />
-          <Scene key='resetPassword' component={ResetPassword} title='Reset Password' />
+          <Scene key='login' component={LogonForm} title={i18n.t("title")} titleStyle={ headerText } />
+          <Scene key='signUp' component={SignUp} title={i18n.t("register")} />
+          <Scene key='resetPassword' component={ResetPassword} title={i18n.t("re_pass")} />
         </Scene>
         <Scene key='rider'>
-          <Scene key='jobList' component={JobList} title={JOB_LIST} /> 
-          <Scene key='riderStats' component={RiderStats} title={JOB_LIST} /> 
+          <Scene key='jobList' component={JobList} title={i18n.t("job_list")} /> 
+          <Scene key='riderStats' component={RiderStats} title={i18n.t("stats")} /> 
         </Scene>
         <Scene key='jobMap'>
-          <Scene key='onJob' component={JobPage} title={WORKING} />
+          <Scene key='onJob' component={JobPage} title={i18n.t("working")} />
         </Scene>
         <Scene key='client'>
-          <Scene key='clientMain' component={CustomerMain} title='Client' />
+          <Scene key='clientMain' component={CustomerMain} title={i18n.t("ride_request")} />
         </Scene>
 
       </Scene>
